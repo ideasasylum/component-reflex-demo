6ba64f7a3e282c449c889086f4d8b009869ff777
Jamie Lawrence <jamie@ideasasylum.com>
2020-05-17 00:05:18 +0100

    Set the view component to render

--- app/reflexes/partial_reflex.rb
+++ app/reflexes/partial_reflex.rb
@@ -1,4 +1,10 @@
 class PartialReflex < StimulusReflex::Reflex
   def random
+    with_component(FastRandomComponent.new)
+  end
+
+  # Would be extracted to a module/superclass
+  def with_component component
+    @component = component
   end
 end
